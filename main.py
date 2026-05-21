1c87d0a97b90262cc116818b6b9511384b13cbab
ChrEber <Chris.Eberlein@proton.me>
2026-05-21 10:16:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,26 @@ with open('items.json','r') as file:
   obj = json.load(file)
   
 loot = random.choice(obj)
+
+itemRarity = ["Common", "Uncommen", "Rare", "Epic", "Legendary"]
+
+rarity = ""
+rarity = random.choice(itemRarity)
+
+valueMulti = 0
+
+if rarity == "Common":
+    valueMulti = 0.5
+elif rarity == "Uncommen":
+    valueMulti = 1
+elif rarity == "Rare":
+    valueMulti = 3
+elif rarity == "Epic":
+    valueMulti = 5
+elif rarity == "Legendary":
+    valueMulti = 10
+
+value = 0.0    
+value = loot["baseValue"] * valueMulti
     
-print(f"Congratulation! You got a {loot["rarity"]} {loot["name"]}. It's worth {loot["value"]} gold!")
+print(f"Congratulation! You got a {rarity} {loot["name"]}. It's worth {value} gold!")
