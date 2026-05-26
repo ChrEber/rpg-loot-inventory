f748c51302aee60689216ff3b6bbecc478118cd7
ChrEber <Chris.Eberlein@proton.me>
2026-05-26 11:04:42 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
+def generate_loot():
+    value = 0
+    loot = roll_loot()
+    rarityGrade = roll_rarity()
+    value = loot["baseValue"] * rarityGrade[0]["valueMulti"]
+    return {
+        "loot": loot["name"],
+        "rarityName": rarityGrade[0]["rarity"],
+        "value": value,
+    }
 
+
+def print_result():
+    getLoot = generate_loot()
+    return f"Congratulation! You got a {getLoot['rarityName']} {getLoot['loot']}. It's worth {getLoot['value']} gold!"
