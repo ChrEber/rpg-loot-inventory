3734ec94d647f14874d94f62d9908476075af8c6
ChrEber <Chris.Eberlein@proton.me>
2026-05-26 09:41:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,56 @@
 import json
 import random
 
-with open('items.json','r') as file:
-  obj = json.load(file)
-  
-loot = random.choice(obj)
+def load_items():
+    with open('items.json','r') as file:
+        items = json.load(file)
+    return items
 
-rarityKeys = ['id', 'rarity', 'itemRarityValue', 'valueMulti', 'rarityColor']
+def roll_loot():
+    item = load_items()
+    loot = random.choice(item)
+    return loot
 
-itemRarity_Liste = {}
-itemRarity_Liste = [
-	[0, "Common",    90, 0.5, "#ffffff"],
-	[1, "Uncommon",  60,   1, "#00ff00"],
-	[2, "Rare",      30,   3, "#0080ff"],
-	[3, "Epic",      10,   5, "#a020f0"],
-	[4, "Legendary",  1,  10, "#ff9900"]
-]
+def build_rarity_data ():
+    itemRarity_Liste = {}
+    rarityKeys = ['id', 'rarity', 'itemRarityValue', 'valueMulti', 'rarityColor']
+    itemRarity_Liste = [
+    	[0, "Common",    90, 0.5, "#ffffff"],
+    	[1, "Uncommon",  60,   1, "#00ff00"],
+    	[2, "Rare",      30,   3, "#0080ff"],
+    	[3, "Epic",      10,   5, "#a020f0"],
+    	[4, "Legendary",  1,  10, "#ff9900"]
+    ]
+    itemRarity_Dict = [dict(zip(rarityKeys, datensatz)) for datensatz in itemRarity_Liste]
+    return itemRarity_Dict
 
-itemRarity_Dict = [dict(zip(rarityKeys, datensatz)) for datensatz in itemRarity_Liste]
+def get_weight():
+    rarity_DB = build_rarity_data()
+    gewichtung = []
+    for i in range(len(rarity_DB)):
+        gewichtung.append(rarity_DB[i]["itemRarityValue"])
+    return gewichtung
 
-gewichtung = []
-for i in range(len(itemRarity_Dict)):
-    gewichtung.append(itemRarity_Dict[i]["itemRarityValue"])
+def roll_rarity():
+    gewichtung = get_weight()
+    itemRarity_Dict = build_rarity_data()
+    rarity = random.choices(itemRarity_Dict, weights = gewichtung, k = 1)
+    return rarity
 
-rarity = random.choices(itemRarity_Dict, weights = gewichtung, k = 1)
-
-value = 0    
-value = loot['baseValue'] * rarity[0]["valueMulti"]
+def generate_loot():
+    value = 0
+    loot = roll_loot()
+    rarityGrade = roll_rarity()
+    value = loot['baseValue'] * rarityGrade[0]["valueMulti"]
+    return {
+        "loot": loot['name'],
+        "rarityName": rarityGrade[0]['rarity'],
+        "value": value
+    }
     
-print(f"Congratulation! You got a {rarity[0]["rarity"]} {loot['name']}. It's worth {value} gold!")
+def print_result():
+    getLoot = generate_loot()
+    return f"Congratulation! You got a {getLoot['rarityName']} {getLoot['loot']}. It's worth {getLoot['value']} gold!"
+    
+print(print_result())
+    
