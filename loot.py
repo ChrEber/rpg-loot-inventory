c3251b832859ebca95109e1e3ef8eda945cf8ea8
ChrEber <Chris.Eberlein@proton.me>
2026-05-26 11:01:19 +0200

    Update loot.py

--- loot.py
+++ loot.py
@@ -1,1 +1,13 @@
+import json
+import random
 
+def load_items():
+    with open("items.json", "r") as file:
+        items = json.load(file)
+    return items
+
+
+def roll_loot():
+    item = load_items()
+    loot = random.choice(item)
+    return loot
