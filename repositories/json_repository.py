5939a730cc8daef8b247d2d4cfdd488f1026ed76
ChrEber <Chris.Eberlein@proton.me>
2026-06-01 14:59:53 +0200

    Update json_repository.py

--- repositories/json_repository.py
+++ repositories/json_repository.py
@@ -1,1 +1,19 @@
+import json
+import os
+from repository_base import Lootrepository
 
+class JsonLootRepository(LootRepository):
+    def __int__(self, filename: str = "item.json"):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.filepath = os.path.join(current_dir, filename)
+
+    def get_all_items(self) -> list[dict]:
+        try:
+            with open(self.filepath, "r", encoding="utf-8") as file:
+                return json.load(file)
+            except FileNotFoundError:
+                print(f"[FEHLER] item.json nicht gefunden unter: {self.filepath}")
+                return []
+            except json.JSONDecodeError:
+                print(f"[FEHLER] Die Datei unter {self.filepath} ist kein gültiges JSON Format!")
+                return []
