5a7a76aaff2880deb25c456b9d38e3d9fbdf0eda
Chris Eberlein <deine@email.de>
2026-07-21 07:52:41 +0200

    icon with rarity

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter
 import data
+import os
 from inventory import InventoryManager
 
 inventory_manager = InventoryManager()
@@ -10,17 +11,43 @@ def update_inventory_display():
     if inventory_frame is not None and inventory_frame.winfo_exists() and outputInventory is not None:
         outputInventory.config(state='normal')
         outputInventory.delete('1.0', tkinter.END)
+        outputInventory.image_labels = []
+
         for item in inventory_manager.inventory:
-            text_zeile = f"{item['rarityName']} | {item['loot']} | Wert: {item['value']} Gold\n"
             farbe = item['rarityColor']
+            
+            icon_label = tkinter.Label(
+                outputInventory, 
+                image=loot_image, 
+                bg=farbe, 
+                borderwidth=0, 
+                highlightthickness=0,
+                padx=0,
+                pady=0
+            )
+            icon_label.image = loot_image
+            outputInventory.image_labels.append(icon_label)
+
+            outputInventory.window_create(
+                tkinter.END, 
+                window=icon_label, 
+                align="center", 
+                padx=0, 
+                pady=0
+            )
+            
+            text_zeile = f"  {item['loot']} | Wert: {item['value']} Gold\n\n"
             outputInventory.tag_config(farbe, foreground=farbe)
             outputInventory.insert(tkinter.END, text_zeile, farbe)
+            
         outputInventory.config(state='disabled')
 
 def roll_button():
     loot = data.generate_loot()
     inventory_manager.addItem(loot)
-    outputText.config(text=f"Congratulation!\n You got a {loot['rarityName']} {loot['loot']}.\n It's worth {loot['value']} gold!", bg=loot["rarityColor"])
+    outputText.config(image=loot_image, text="", bg=loot["rarityColor"])
+        #outputText.config(text=f"Congratulation!\n You got a {loot['rarityName']} {loot['loot']}.\n It's worth {loot['value']} gold!", bg=loot["rarityColor"])
+    outputText.image = loot_image
 
     update_inventory_display()
 
@@ -42,6 +69,10 @@ def show_inventory():
     update_inventory_display()
 
 root = tkinter.Tk()
+
+items_bild = os.path.join('assets', 'Allgemeines Icon.png')
+loot_image = tkinter.PhotoImage(file=items_bild)
+
 root.title('RPG Loot Generator')
 root.configure(background = 'black')
 root.eval('tk::PlaceWindow . center')
